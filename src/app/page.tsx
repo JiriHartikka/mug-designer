e490220df178f67cf48c6e28b4a82c86d75f60e9
Jiri Hartikka <jiri.hartikka@gmail.com>
2024-01-01 12:09:20 +0200

    Set Next.js output type to export. Fix ssr and module resolution problems with three.js fiber

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
 'use client';
 
 import styles from './page.module.css'
-import MugScene from '@/three/mug-scene';
 import ImageInput from '@/components/file-input/image-input';
-import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import { Suspense, useState } from 'react';
+
+const MugScene = dynamic(() => import('@/three/mug-scene'), {
+  ssr: false,
+});
 
 export default function Home() {
 
   const [image, setImage] = useState<HTMLImageElement>();
 
   return (
-    <main className={styles.main}>
-      <div className={styles.canvasContainer}>
-        <MugScene textureImage={image} />
-      </div>
+    <Suspense>
+      <main className={styles.main}>
+        <div className={styles.canvasContainer}>
+          <MugScene textureImage={image} />
+        </div>
 
-      <div>
-        <ImageInput onImageUploaded={setImage} />
-      </div>
-    </main>
+        <div>
+          <ImageInput onImageUploaded={setImage} />
+        </div>
+      </main>
+    </Suspense>
   )
 }
